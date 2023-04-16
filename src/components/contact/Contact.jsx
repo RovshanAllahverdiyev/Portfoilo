eb7b848ee885bff742f5511c79817e3a500c0e2b
Rovshan13 <allakhverdievrovshann@gmail.com>
2023-04-16 15:35:57 +0400

    created: Responsive Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React ,{useRef} from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7fb3rlg', 'template_94ffchs', form.current, 'jEYvSwYBHqoxtwxI9')
+      
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>allakhverdievrovshann@gmail.com</h5>
+            <a href="mailto:allakhverdievrovshann@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+994(55)423-33-53</h5>
+            <a href="https://api.whatsapp.com/send?phone=994554233353" target='_blank'>Send a message</a>
+          </article>
+
+          
+        </div>
+
+        {/* END OF CONTACT OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
